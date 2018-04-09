7f3d8a884b4dc2e5109475a6b6212a32ce7a89b2
TurtleLiu <38153198+TurtleLiu@users.noreply.github.com>
2018-04-09 21:16:17 +0800

    Create CheckRepete.h

---
+++ CheckRepete.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef __CHECKREPETE_H_
+#define __CHECKREPETE_H_
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool Check_Rep(const vector<int> &Seed_Box);
+
+#endif
