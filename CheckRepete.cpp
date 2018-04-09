7724f6c22f62a3f45030fcfb896bb5ac8dcac064
TurtleLiu <38153198+TurtleLiu@users.noreply.github.com>
2018-04-09 21:15:46 +0800

    Create CheckRepete.cpp

---
+++ CheckRepete.cpp
@@ -0,0 +1,28 @@
+#include "CheckRepete.h"
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int seedku[10000][8];
+int finalsudo = 0;
+
+bool Check_Rep(const vector<int> &Seed_Box)
+{
+	for (int i = finalsudo - 1; i > -1; i--)
+	{
+		for (int j = 0, count = 8; j < 8; j++)
+		{
+			if (Seed_Box[j + 1] == seedku[i][j]) count--;
+			else 
+				break;
+			if (count == 0) 
+				return false;
+		}
+	}
+
+	for (int i = 1; i < 9; i++)
+		seedku[finalsudo][i - 1] = Seed_Box[i];
+
+	finalsudo++;
+	return true;
+}
