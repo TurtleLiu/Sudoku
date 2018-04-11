6b122fb133b1cea5387c8f4e24d9e237ffae4fdd
TurtleLiu <38153198+TurtleLiu@users.noreply.github.com>
2018-04-11 22:48:54 +0800

    Create Solve.h

---
+++ Solve.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef __SOLVE_H_
+#define __SOLVE_H_
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+void remove(int c);
+void resume(int c);
+bool dfs();
+bool build();
+void print();
+void SolveSudo(char x[83]);
+#endif
+
