bd577a2c2a71d1c6b112c3e0577d29c48b75a44c
TurtleLiu <38153198+TurtleLiu@users.noreply.github.com>
2018-04-15 10:58:40 +0800

    Change: Use a faster way to bulid the loop

--- Solve.h
+++ Solve.h
@@ -4,12 +4,9 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <cstdlib>
+#inlcude <algorithm>
 using namespace std;
-void remove(int c);
-void resume(int c);
-bool dfs();
-bool build();
-void print();
 void SolveSudo(char x[83]);
 #endif
 
